26de8fe4b14311fd037728fe832b3b8536901df6
João Oliveira <jmlcoliveira@gmail.com>
2023-10-10 12:51:52 +0100

    X and Y reading

--- project.py
+++ project.py
@@ -43,21 +43,31 @@ def load_test_values(file_name):
                      ])
     return np.array(rows)
 
-train_data = load_train_values('X_train.csv')
-test_data = load_test_values('X_test.csv')
+train_data = pd.read_csv("X_train.csv")
+# train_data = load_train_values('X_train.csv')
+# test_data = pd.read_csv("X_test.csv")
+# test_data = load_test_values('X_test.csv')
+X_train = []
+Y_train = []
+baseX = None
+for line in train_data.values:
+    if line[0] == 0:
+        baseX = [line[0], line[1], line[2], line[5], line[6], line[9], line[10]]
+    X_train.append([line[0], baseX[1], baseX[2], baseX[3], baseX[4], baseX[5], baseX[6]])
+    Y_train.append([line[1], line[2], line[5], line[6], line[9], line[10]])
 
 plt.figure()
 scaler = StandardScaler()
 train_data=scaler.fit(train_data).transform(train_data)
 print(train_data.min(), train_data.max())
 
-X_train = train_data[:, 1:]  # Features (columns 1 onwards)
-y_train = train_data[:, 0]   # Target variable (timestep, column 0)
+# X_train = train_data[:, 1:]  # Features (columns 1 onwards)
+# y_train = train_data[:, 0]   # Target variable (timestep, column 0)
 
-X_test = test_data[:, 1:]    # Features for test data
+# X_test = test_data[:, 1:]    # Features for test data
 
 model = LinearRegression()
-model.fit(X_train, y_train)
+model.fit(X_train, Y_train)
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
